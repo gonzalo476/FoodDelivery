04d4f4ab559512a5dcd246ebd2cce501acfe22a5
Gonzalo Rojas <rojasg476@gmail.com>
2020-12-14 17:00:04 -0600

    initial

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {Restaurant, OrderDelivery, Home} from './screens';
+import Tabs from './navigation/tabs';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+    if (!fontsLoaded) {
+      loadFonts();
+    }
+  })
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      "Gellix-Regular": require("./assets/fonts/Gellix-Regular.otf"),
+      "Gellix-Medium": require("./assets/fonts/Gellix-Medium.otf"),
+      "Gellix-SemiBold": require("./assets/fonts/Gellix-SemiBold.otf"),
+      "Gellix-Bold": require("./assets/fonts/Gellix-Bold.otf"),
+      "Gellix-Black": require("./assets/fonts/Gellix-Black.otf"),
+    });
+    setFontsLoaded(true);
+  }
+
+  if (!fontsLoaded){
+    return <View/>
+  }
+    return (
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName={"Home"}
+      >
+        <Stack.Screen name="Home" component={Tabs}/>
+        <Stack.Screen name="Restaurant" component={Restaurant}/>
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
